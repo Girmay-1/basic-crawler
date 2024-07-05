4e731be0bc42c53d4b40012913a35cc8b2b34b22
Girmay-1 <girmaykibreab93@gmail.com>
2024-07-05 14:17:05 -0700

    added a main method

---
+++ main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        String startUrl = "http://example.com";
+        int maxDepth = 3;
+        WebCrawler crawler = new WebCrawler(startUrl,maxDepth);
+        crawler.startCrawling();
+    }
+}
